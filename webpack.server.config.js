4f62b21021a74008cb5a0a4a6a4819bbe4109dc1
Hernán Magrini <hernan.magrini@klarna.com>
2019-03-15 14:04:05 +0100

    fix(server): add ModuleMapLoader correctly on server and server module

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -2,19 +2,27 @@
 
 const path = require('path');
 const webpack = require('webpack');
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
+  resolve: {
+    extensions: ['.js', '.ts'],
+    plugins: [
+      new TsconfigPathsPlugin({configFile: path.join(process.cwd(),'apps/myworkspace/tsconfig.server.json')})
+    ]
+  },
+  target: 'node',
+  externals: [/(node_modules|main\..*\.js)/],
+  optimization: {
+    minimize: false
+  },
+  node: {
+    __dirname: true
+  },
   mode: 'none',
   entry: {
     // This is our Express server for Dynamic universal
     server: './server.ts'
-  },
-  target: 'node',
-  resolve: {
-    extensions: ['.js', '.ts']
-  },
-  optimization: {
-    minimize: false
   },
   output: {
     // Puts the output at the root of the dist folder
@@ -23,24 +31,30 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.ts$/, loader: 'ts-loader' },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          configFile: path.join(process.cwd(), `apps/myworkspace/tsconfig.server.json`)
+        }
+      },
       {
         // Mark files inside `@angular/core` as using SystemJS style dynamic imports.
         // Removing this will cause deprecation warnings to appear.
         test: /(\\|\/)@angular(\\|\/)core(\\|\/).+\.js$/,
-        parser: { system: true },
-      },
+        parser: { system: true }
+      }
     ]
   },
   plugins: [
+    // Temporary Fix for issue: https://github.com/angular/angular/issues/11580
+    // for 'WARNING Critical dependency: the request of a dependency is an expression"
     new webpack.ContextReplacementPlugin(
-      // fixes WARNING Critical dependency: the request of a dependency is an expression
       /(.+)?angular(\\|\/)core(.+)?/,
       path.join(__dirname, 'src'), // location of your src
       {} // a map of your routes
     ),
     new webpack.ContextReplacementPlugin(
-      // fixes WARNING Critical dependency: the request of a dependency is an expression
       /(.+)?express(\\|\/)(.+)?/,
       path.join(__dirname, 'src'),
       {}
